d5ee221fbd448ed3e560d9c150dc3bfc04e20f11
ATMOSPHERE <sannnd@yeah.net>
2024-06-27 03:20:57 +0800

    Revert "Create cookie_write.js"
    This reverts commit a5b7df9ea900c7fac209a6dd6fe5135b75c36852.

--- module/cookie_write.js
+++
@@ -1,9 +0,0 @@
-const storage = require('../app'); // 根据实际路径调整
-
-async function writeUserCookie(cookie) {
-  await storage.setItem('cookie', cookie);
-}
-
-module.exports = {
-  writeUserCookie,
-};
