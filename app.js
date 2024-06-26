504b6b63fa58de6bae004f844d1db713244ef367
ATMOSPHERE <sannnd@yeah.net>
2024-06-27 03:20:36 +0800

    Revert "persist cookie"
    This reverts commit 2a423eb16bc66ac1100b28406f521f2fc42613ba.

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const fs = require('fs')
 const path = require('path')
 const tmpPath = require('os').tmpdir()
-const storage = require('node-persist');
 
 async function start() {
   // 检测是否存在 anonymous_token 文件,没有则生成
@@ -15,16 +14,5 @@ async function start() {
   require('./server').serveNcmApi({
     checkVersion: true,
   })
-  // 初始化 node-persist
-  storage.init({
-    dir: './persist', // 存储文件的目录
-    stringify: JSON.stringify,
-    parse: JSON.parse,
-    encoding: 'utf8',
-    logging: false, // 是否启用日志
-    continuous: true, // 是否实时写入
-    interval: false, // 实时写入的时间间隔
-    ttl: false // 数据的存活时间（毫秒）
-  });
 }
 start()
