257c7e8ec3b081892fdfffcca255d240343033f6
ATMOSPHERE <sannnd@yeah.net>
2024-06-27 11:00:33 +0800

    Update cookie.js

--- module/cookie.js
+++ module/cookie.js
@@ -1,4 +1,4 @@
-//更新后端Cookie
+//更新Cookie ENV
 module.exports = async (query, fetch) => {
 
     const body = JSON.stringify({
@@ -20,7 +20,7 @@ module.exports = async (query, fetch) => {
             },
         ],
     });
-    
+
     const updateEdgeConfig = await fetch(
         'https://api.vercel.com/v1/edge-config/ecfg_s5mlaclrjfl2nzclxmkibfpvtntm/items',
         {
@@ -34,4 +34,24 @@ module.exports = async (query, fetch) => {
     );
     const result = await updateEdgeConfig.json();
     console.log(result);
+    if (response.ok) {
+        const result = await response.json();
+        // 返回成功响应
+        res.status(200).json({
+          status: 200,
+          body: {
+            code: 200,
+            data: result,
+          },
+        });
+      } else {
+        // 返回错误响应
+        res.status(response.status).json({
+          status: response.status,
+          body: {
+            code: response.status,
+            message: 'Failed to update Edge Config',
+          },
+        });
+      }
 }
