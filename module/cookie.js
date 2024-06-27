616b9af50f94614a8aba85de8761696095ca3c5d
ATMOSPHERE <sannnd@yeah.net>
2024-06-27 11:57:35 +0800

    Update cookie.js

--- module/cookie.js
+++ module/cookie.js
@@ -19,25 +19,29 @@ module.exports = async (query, fetch) => {
             },
         ],
     });
-    const res = await fetch(
-        'https://api.vercel.com/v1/edge-config/ecfg_s5mlaclrjfl2nzclxmkibfpvtntm/items?teamId=team_9Cg1Z5LRvs1c2wAxsNclmLeU',
+    try {
+        const res = await fetch(
+        'https://api.vercel.com/v1/edge-config/ecfg_s5mlaclrjfl2nzclxmkibfpvtntm/items',
         {
             method: 'PATCH',
             headers: {
-                Authorization: `Bearer ` + process.env.VERCEL_ACCESS_TOKEN,
+                Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`,
                 'Content-Type': 'application/json',
             },
             body,
         },
-    );
-    console.log("finish");
-    const result = await res.json();
-    console.log(result);
-    return {
+        );
+        console.log("finish");
+        const result = await res.json();
+        return {
         status: 200,
         body: {
           code: 200,
           data: result,
         },
     }
+    } catch (error) {
+        console.log(error);
+    }
+    
 }
