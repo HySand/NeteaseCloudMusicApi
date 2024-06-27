0bae8c32f2dd31aca6bd24048e887a2a65a0182a
ATMOSPHERE <sannnd@yeah.net>
2024-06-27 09:43:07 +0800

    update

--- module/cookie.js
+++ module/cookie.js
@@ -1,9 +1,5 @@
 //更新后端Cookie
 module.exports = async (query, request) => {
-    const c1 = await get('__csrf');
-    const c2 = await get('MUSIC_U');
-    const c3 = await get('NMTID');
-    
     const res = await fetch(
         'https://api.vercel.com/v1/edge-config/ecfg_s5mlaclrjfl2nzclxmkibfpvtntm/items',
         {
@@ -17,17 +13,17 @@ module.exports = async (query, request) => {
                     {
                         operation: 'update',
                         key: '__csrf',
-                        value: query.cookie('__csrf'),
+                        value: query.cookie['__csrf'],
                     },
                     {
                         operation: 'update',
                         key: 'MUSIC_U',
-                        value: query.cookie('MUSIC_U'),
+                        value: query.cookie['MUSIC_U'],
                     },
                     {
                         operation: 'update',
                         key: 'NMTID',
-                        value: query.cookie('NMTID'),
+                        value: query.cookie['NMTID'],
                     }
                 ],
             }),
