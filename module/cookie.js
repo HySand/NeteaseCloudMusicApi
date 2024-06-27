3bfbf50fc98b84691a9c95784dd58331d9ad98d8
ATMOSPHERE <sannnd@yeah.net>
2024-06-27 11:02:56 +0800

    Update cookie.js

--- module/cookie.js
+++ module/cookie.js
@@ -1,4 +1,6 @@
 //更新Cookie ENV
+const fetch = require('node-fetch');
+
 module.exports = async (query, fetch) => {
 
     const body = JSON.stringify({
@@ -34,24 +36,11 @@ module.exports = async (query, fetch) => {
     );
     const result = await updateEdgeConfig.json();
     console.log(result);
-    if (response.ok) {
-        const result = await response.json();
-        // 返回成功响应
-        res.status(200).json({
-          status: 200,
-          body: {
-            code: 200,
-            data: result,
-          },
-        });
-      } else {
-        // 返回错误响应
-        res.status(response.status).json({
-          status: response.status,
-          body: {
-            code: response.status,
-            message: 'Failed to update Edge Config',
-          },
-        });
-      }
+    return {
+        status: 200,
+        body: {
+          code: 200,
+          data: result,
+        },
+    }
 }
