6c2f85b92b5b0cda03166daa4dfe8f291f862b62
ATMOSPHERE <sannnd@yeah.net>
2024-06-27 10:37:24 +0800

    Update cookie.js

--- module/cookie.js
+++ module/cookie.js
@@ -1,43 +1,37 @@
 //更新后端Cookie
-    try {
-        const updateEdgeConfig = await fetch(
-          'https://api.vercel.com/v1/edge-config/ecfg_s5mlaclrjfl2nzclxmkibfpvtntm/items',
-          {
+module.exports = async (query, fetch) => {
+
+    const body = JSON.stringify({
+        items: [
+            {
+                operation: 'update',
+                key: '__csrf',
+                value: 'v1',
+            },
+            {
+                operation: 'update',
+                key: 'MUSIC_U',
+                value: 'v2',
+            },
+            {
+                operation: 'update',
+                key: 'NMTID',
+                value: 'v3',
+            },
+        ],
+    });
+    
+    const updateEdgeConfig = await fetch(
+        'https://api.vercel.com/v1/edge-config/ecfg_s5mlaclrjfl2nzclxmkibfpvtntm/items',
+        {
             method: 'PATCH',
             headers: {
-              Authorization: `Bearer `+ process.env.VERCEL_ACCESS_TOKEN,
-              'Content-Type': 'application/json',
+                Authorization: `Bearer ${process.env.VERCEL_ACCESS_TOKEN}`,
+                'Content-Type': 'application/json',
             },
-            body: JSON.stringify({
-              items: [
-                {
-                  operation: 'update',
-                  key: '__csrf',
-                  value: 'v1',
-                },
-                {
-                  operation: 'update',
-                  key: 'MUSIC_U',
-                  value: 'v2',
-                },
-                {
-                  operation: 'update',
-                  key: 'NMTID',
-                  value: 'v3',
-                },
-              ],
-            }),
-          },
-        );
-        const result = await updateEdgeConfig.json();
-        console.log(result);
-      } catch (error) {
-        console.log(error);
-      }
-      return {
-        status: 200,
-        body: {
-          code: 200,
-          data: result,
+            body,
         },
-      }
+    );
+    const result = await updateEdgeConfig.json();
+    console.log(result);
+}
