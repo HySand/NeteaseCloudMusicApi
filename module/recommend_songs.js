734650cf599c788e5831fc9f3e7544156d7e21f2
ATMOSPHERE <sannnd@yeah.net>
2024-09-28 18:02:58 +0800

    Update recommend_songs.js

--- module/recommend_songs.js
+++ module/recommend_songs.js
@@ -1,7 +1,13 @@
 // 每日推荐歌曲
 import { get } from '@vercel/edge-config';
 
-module.exports = (query, request) => {
+module.exports = async (query, request) => {
+  const c1 = await get('__csrf');
+  const c2 = await get('MUSIC_U');
+  const c3 = await get('NMTID');
+  query.cookie.__csrf = c1;
+  query.cookie.MUSIC_U = c2;
+  query.cookie.NMTID = c3;
   query.cookie.os = 'ios'
   const data = {}
   return request(
